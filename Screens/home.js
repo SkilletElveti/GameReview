35a2ef6b2f277ad585492c097abd351c715884c5
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-29 16:25:22 -0700

    - Added Navigation to home.js

--- Screens/home.js
+++ Screens/home.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text,Button } from 'react-native';
+
 import { globalStyles } from '../styles/global';
-export default function Home() {
+export default function Home({navigation}) {
+    const onPressHandler = () => {
+        navigation.navigate('Details');
+    }
     return (<View style={globalStyles.container}>
         <Text style={globalStyles.titelText}>Home Screen</Text>
+        <Button title = "Navigate" onPress={() => {onPressHandler()}}/>
     </View>);
 }
 
