fbe3f20f33c7a2f977940a851f60474dc1bbf05d
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-29 15:36:56 -0700

    - Bug Fixes

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './Screens/home';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import Apploading from "expo-app-loading";
+import Home from "./Screens/home";
 
-export default function App() {
-  return (
-    <Home />
-  );
-}
+const getFonts = () =>
+  Font.loadAsync({
+    regular: require("./assets/fonts/Nunito-Regular.ttf"),
+    bold: require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
+  export default function App() {
+    const [fontsloaded, setFontsLoaded] = useState(false);
+  
+    if (fontsloaded) {
+      return (
+       //you can return any component of your choice here
+        <Home/>
+      );
+    } else {
+      return (
+        <Apploading
+          startAsync={getFonts}
+          onFinish={() => {
+            setFontsLoaded(true);
+          }}
+          onError={console.warn}
+        />
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    Texts: {
+      fontFamily: "bold",
+    },
+  });
+  
