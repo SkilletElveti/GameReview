584c9725407d125337908bc1b2d963eae059a63e
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-29 15:36:46 -0700

    - Bug Fixes

--- Screens/home.js
+++ Screens/home.js
@@ -1,22 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import * as Font from 'expo-font'
-const getFonts = () => {
-    return Font.loadAsync({
-        'nunito-regular': require('./assets/Fonts/Nunito-Regular.ttf'),
-        'nunito-bold': require('./assets/Fonts/Nunito-Bold.ttf'),
-    })
-}
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 export default function Home() {
-    return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-        </View>
-    );
+    return (<View style={styles.container}>
+        <Text style={styles.container}>Home Screen</Text>
+    </View>);
 }
 
 const styles = StyleSheet.create({
     container: {
         padding: 24,
+        fontFamily: 'bold',
     },
 });
