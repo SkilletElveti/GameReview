d030469898c386044f031a69d3ed81133c4d43a9
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-28 18:29:12 -0700

    - Added Fonts

--- Screens/home.js
+++ Screens/home.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
+import * as Font from 'expo-font'
+const getFonts = () => {
+    return Font.loadAsync({
+        'nunito-regular': require('./assets/Fonts/Nunito-Regular.ttf'),
+        'nunito-bold': require('./assets/Fonts/Nunito-Bold.ttf'),
+    })
+}
 export default function Home() {
     return (
         <View style={styles.container}>
