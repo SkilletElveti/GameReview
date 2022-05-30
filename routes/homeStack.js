5a5d598d2b0a329724d7ac1246865f4dc2869b0c
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-30 16:37:44 -0700

    - Added header Styles

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,14 +5,32 @@ import Details from "../Screens/details";
 import About from "../Screens/about";
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'GameZone',
+            headerStyle: {
+                //backgroundColor: '#eee',
+            }
+        },
     },
     Details: {
-        screen: Details
+        screen: Details,
+        navigationOptions: {
+            title: 'Review Details',
+            headerStyle: {
+                //backgroundColor: '#eee',
+            }
+        },
     },
     About: {
         screen: About
     },
 }
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+     defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: {
+            backgroundColor: '#eee',
+            height: 60
+        }}});
 export default createAppContainer(HomeStack);
