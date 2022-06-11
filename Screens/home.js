6504b79326efbfe271ae492a47f4cc59f85d2237
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-06-10 17:10:33 -0700

    - Added Modal - Added Review Form

--- Screens/home.js
+++ Screens/home.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity, FlatList, Modal } from 'react-native';
 import Card from '../shared/Card';
-
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { globalStyles } from '../styles/global';
 export default function Home({ navigation }) {
+    const [modelOpen, setModelOpen] = useState(false);
     const [reviews, setReviews] = useState([
         { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
         { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
@@ -14,6 +15,27 @@ export default function Home({ navigation }) {
         navigation.navigate('Details');
     }
     return (<View style={globalStyles.container}>
+        <Modal visible={modelOpen} animationType='slide'>
+            <View>
+                <MaterialIcons
+                    name='close'
+                    size={24}
+                    onPress={() => {
+                        setModelOpen(false)
+                    }}
+                    style={styles.modalInner}
+                />
+                <Text> Hello from Modal :) </Text>
+            </View>
+        </Modal>
+        <MaterialIcons
+            name='add'
+            size={24}
+            onPress={() => {
+                setModelOpen(true)
+            }}
+            style={styles.modalToggle}
+        />
         <FlatList
             data={reviews}
             renderItem={({ item }) => {
@@ -31,3 +53,26 @@ export default function Home({ navigation }) {
     </View>);
 }
 
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginTop:0,
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#2222',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalInner: {
+        marginTop:40,
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#2222',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    }, 
+    modalContent: {
+        
+    }
+})
